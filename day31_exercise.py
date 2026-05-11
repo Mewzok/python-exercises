552ded4454bf725a439fc0356f49a5de6f018515
mewzok7@yahoo.com <mewzok7@yahoo.com>
2026-05-11 03:52:38 -0600

    -Add day 31 exercise.

---
+++ day31_exercise.py
@@ -0,0 +1,21 @@
+def add_purchase(users, username, amount):
+    if amount > 0:
+        users.setdefault(username, {"total_spent": 0, "purchase_count": 0})
+        users[username]["total_spent"] += amount
+        users[username]["purchase_count"] += 1
+
+    return users
+
+users = {
+    "alice": {
+        "total_spent": 50,
+        "purchase_count": 2
+    },
+    "bob": {
+        "total_spent": 15,
+        "purchase_count": 1
+    }
+}
+
+print(add_purchase(users, "alice", 25))
+print(add_purchase(users, "charlie", 40))
